25a6db1f32d90063b3c16d361eb03c87f1780647
Serhii Verbitskiy <sjcris.me@gmail.com>
2017-12-07 11:50:42 +0200

    add author data

--- src/app/todolist.data.ts
+++ src/app/todolist.data.ts
@@ -1,4 +1,5 @@
 import { todos } from './modal/todos.modal';
+import { authour } from './modal/authour.modal';
 
 export let todosListData: [todos] = [
   { id: 1, message: 'Hello one ', authour: 1, price: 1 },
@@ -6,4 +7,10 @@ export let todosListData: [todos] = [
   { id: 3, message: 'Gofor the work one ', authour: 3, price: 10 },
   { id: 5, message: 'feed the fish ', authour: 3, price: 5 },
   { id: 6, message: 'test this app ', authour: 2, price: 2 },
+];
+
+export let authours: [authour] = [
+  { id: 1, name: 'admin' },
+  { id: 2, name: 'user' },
+  { id: 3, name: 'tester' }
 ];
